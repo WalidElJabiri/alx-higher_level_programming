d0d0efd91f75d2f8f87c76d6b832151d76ecef81
walid <jabiriwalid555@gmail.com>
2023-06-09 07:05:35 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("{} arguments.".format(len(sys.argv) - 1))
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+    print("{}: {}".format(1, sys.argv[1]))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    i = 1
+    while i < len(sys.argv):
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
