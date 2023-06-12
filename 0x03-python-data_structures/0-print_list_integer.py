9e8e0e0b1264973fcc3b212887f3a766f241efac
walid <jabiriwalid555@.com>
2023-06-12 17:58:06 +0000

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list):
+# 0-print_list_integer.py
+def print_list_integer(my_list=[]):
     i = 0
-    for i in range(len(my_list=[])):
+    for i in range(len(my_list)):
 	print("{}".format(my_list[i]))
