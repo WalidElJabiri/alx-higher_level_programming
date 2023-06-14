06411c1ba8b6b08ab968ab84f9fde7674222291a
walid <jabiriwalid555@.com>
2023-06-14 23:16:47 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,15 +2,12 @@
 def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
     lis = list(new_dir.keys())
-
     for x in lis:
         new_dir[x] *= 2
     return (new_dir)
-        
+
 def print_sorted_dictionary(a_dictionary):
     sor = list(a_dictionary.keys())
     sor.sort()
-
     for x in sor:
         print("{}: {}".format(x, a_dictionary[x]))
-
