317c56d581be1ff18a23fb3fe28501d89bbe1bc4
walid <jabiriwalid555@.com>
2023-06-14 22:15:48 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
+
 def print_sorted_dictionary(a_dictionary):
     sor = list(a_dictionary.keys())
     sor.sort()
