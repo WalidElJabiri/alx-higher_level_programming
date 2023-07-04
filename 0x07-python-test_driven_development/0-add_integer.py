22433ce0f31fe491c84278ab8203c76ef1902a5a
walid <jabiriwalid555@.com>
2023-07-04 13:08:29 +0000

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+function that adds two numbers
+
+"""
+
+def add_integer(a, b=98):
+    """ Function that adds two int and/or float numbers
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
