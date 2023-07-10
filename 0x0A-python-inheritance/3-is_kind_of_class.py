336a3cb059465faad71cc89dac711961b0536bc5
walid <jabiriwalid555@.com>
2023-07-10 17:21:02 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is kind of a class"""
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module
+"""
+class BaseGeometry:
+    """BaseGeometry"""
+    @classmethod
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
