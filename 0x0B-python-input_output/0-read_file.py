0d3db2bccae0e0d4597998453ff75dc92d6be8e9
walid <jabiriwalid555@.com>
2023-07-11 22:53:58 +0000

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""
+module
+"""
 def read_file(filename=""):
     """read file"""
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    with open(filename, 'r') as fl:
+        print(fl.read(), end="")
  
