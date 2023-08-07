715687fad6e013fd58874f814291906ffdef931a
walid <jabiriwalid555@gmail.com>
2023-08-07 17:54:08 +0100

    Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+square mpdule
+"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        self.size = size
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
