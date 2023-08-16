46a8ba309c71bb583190debfe2c5cd3f8f8a4023
walid <jabiriwalid555@.com>
2023-08-16 17:50:26 +0000

    adding files

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0c_0 on MySQL server,
+-- do nothing if the hbtn_0c_0 exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database hbtn_0c_0 from MySQL server,
+-- do nothing if hbtn_0c_0 doesn't exist
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
