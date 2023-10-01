3116d0532485aea1f6f527aa6a0429d98e891a2b
walid <jabiriwalid555@gmail.com>
2023-10-01 17:15:49 +0100

    adding tasks

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     st_name = sys.argv[4]
 
     url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-            'username': mySQL_user, 'password': mySQL_pass, 'database': db_name}
+           'username': mySQL_user, 'password': mySQL_pass, 'database': db_name}
 
     engine = create_engine(URL(**url), pool_pre_ping=True)
     Base.metadata.create_all(engine)
