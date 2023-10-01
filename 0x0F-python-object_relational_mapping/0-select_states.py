22cdd6b2ab2d6125661806a89f5ec1408bae95d2
walid <jabiriwalid555@gmail.com>
2023-10-01 15:10:39 +0100

    adding first task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySQL_user = sys.argv[1]
+    mySQL_pass = sys.argv[2]
+    db_name = sys.argv[3]
+
+        db = MySQLdb.connect(user=mySQL_user, passwd=mySQL_pass, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
