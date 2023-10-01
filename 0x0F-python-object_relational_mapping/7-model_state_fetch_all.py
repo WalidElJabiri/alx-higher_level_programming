e1499b542544f72459fdc56acf737c61f47d8ded
walid <jabiriwalid555@gmail.com>
2023-10-01 17:07:51 +0100

    adding tasks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-            'username': mySQL_user, 'password': mySQL_pass,
+        'username': mySQL_user, 'password': mySQL_pass,
             'database': db_name}
 
     engine = create_engine(URL(**url), pool_pre_ping=True)
