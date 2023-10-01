1a3b8952c131dc333b705f8419dbd781ea568a2f
walid <jabiriwalid555@gmail.com>
2023-10-01 16:20:55 +0100

    adding tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
-            .format(search_name))
+                .format(search_name))
     rows = cur.fetchall()
 
     for row in rows:
