bdada08026920b8bcf748c13d0c1cfafc85f0816
walid <jabiriwalid555@gmail.com>
2023-10-25 01:18:42 +0100

    adding tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/planets/1/${id}`;
 
 request(url, function (err, response, body) {
   if (err) {
